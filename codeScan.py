f4233bd1c90690f6a78da06a937664a44a8260c2
usama.latif <usama.latif@cgi.com>
2024-10-29 20:55:19 +0100

    code test

--- codeScan.py
+++ codeScan.py
@@ -5,7 +5,4 @@ def list_directory(directory):
     command = f"ls {directory}"
     subprocess.run(command, shell=True)
 
-def insufficient_permissions():
-    # File operation without sufficient permissions
-    with open("/etc/passwd", "w") as file:  # Potentially dangerous file write operation
-        file.write("New user")
+
