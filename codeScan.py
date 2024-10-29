d0b627eafc79d41630f52723c106e2003adb2ec8
usama.latif <usama.latif@cgi.com>
2024-10-29 21:29:32 +0100

    test

--- codeScan.py
+++ codeScan.py
@@ -11,21 +11,5 @@ def insecure_sql_injection(user_id):
     query = "SELECT * FROM users WHERE id = '" + user_id + "'"
     cursor.execute(query)  # Vulnerable to SQL Injection
 
-def hardcoded_secret_key():
-    # Hardcoded secret
-    secret_key = "my_super_secret_key_1234"
-
-def unsafe_deserialization(data):
-    # Unsafe deserialization with pickle
-    user_data = pickle.loads(data)  # Vulnerable to code injection attacks
-
-def weak_hashing(password):
-    # Weak hashing algorithm
-    hashed_password = hashlib.md5(password.encode()).hexdigest()  # MD5 is insecure
-
-def run_shell_command():
-    # Shell injection vulnerability
-    command = "ls " + input("Enter directory: ")
-    subprocess.run(command, shell=True)  # Vulnerable to shell injection
 
 
