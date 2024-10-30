747fd283fdf296d6c80b18d94d89932c41928483
usama.latif <usama.latif@cgi.com>
2024-10-30 18:49:19 +0100

    vulnerable code added

--- codeScan.py
+++ codeScan.py
@@ -6,3 +6,22 @@ def list_directory(directory):
     subprocess.run(command, shell=True)
 
 
+from jinja2 import Template
+
+def render_template(template_string, context):
+
+    template = Template(template_string)
+    return template.render(context)
+
+
+class MyClass:
+    def method_a(self):
+        print("Method A")
+
+    def method_b(self):
+        print("Method B")
+
+def call_method(obj, method_name):
+
+    method = getattr(obj, method_name)
+    method()
