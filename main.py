dbe6ce4c76aa3cf8d1b827b89b81d6e8cd481ffd
usama.latif <usama.latif@cgi.com>
2024-12-10 14:57:17 +0100

    bug fix

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ from agents.vulnerability_agent import handle_vulnerability,  generate_markdown_
 
 
 def main():
-    # Call handle_vulnerability to get the scan report
-    issues = handle_vulnerability()
-    issues = [issue for issue in issues if issue]
-  #  Generate a Markdown report if there are any issues
-    if not issues:
-        print("No vulnerabilities found.")
 
+    issues = handle_vulnerability()
+    if isinstance(issues, dict) and not issues.get("issues"):
+        print("No vulnerabilities found.")
     else:
         generate_markdown_report(issues)
 
