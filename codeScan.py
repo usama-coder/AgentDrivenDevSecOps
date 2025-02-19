06b015606c0c5ff7dcd89d935b0dca44cc24e697
usama.latif <usama.latif@cgi.com>
2025-02-19 16:20:51 +0100

    testing detect-secrets

--- codeScan.py
+++ codeScan.py
@@ -13,9 +13,6 @@ pattern.match("aaaaaaaaaaaaaaaaaaaa!")
 
 
 
-import xml.etree.ElementTree as ET
-xml_data = "<!DOCTYPE foo [<!ENTITY xxe SYSTEM 'file:///etc/passwd'>]> <foo>&xxe;</foo>"
-root = ET.fromstring(xml_data)  # 🚨 XXE Vulnerability
 
 
 AWS_ACCESS_KEY_ID = "AKIAEXAMPLE1234567890"
