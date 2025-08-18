cd7020addd310badf48a54f967ed33950fd800ad
usamalatif <latif@rhrk.uni-kl.de>
2025-08-18 19:12:02 +0200

    errors infused

--- codeScan.py
+++ codeScan.py
@@ -10,7 +10,7 @@ def sqlQuery():
 
 
 def connect_database():
-    password = "rootjklfdgasasssss"
+    password = "rootjklfdgasasassasssss"
 
 def create_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
