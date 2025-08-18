9a6386b36c05bb39bfaedfcb1c923d6715c985c6
usamalatif <latif@rhrk.uni-kl.de>
2025-08-18 18:59:02 +0200

    infused error

--- codeScan.py
+++ codeScan.py
@@ -10,7 +10,7 @@ def sqlQuery():
 
 
 def connect_database():
-    password = "rootjklfdg"
+    password = "rootjklfdgssss"
 
 def create_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
